ba12e88c10a2d877ec3c1944ec9d9f86270e2a91
cassiekang <143849077+cassiekang@users.noreply.github.com>
2023-10-03 17:44:12 +0000

    initial commmit

--- mylib/query.py
+++ mylib/query.py
@@ -2,15 +2,104 @@
 
 import sqlite3
 
+# Define a global variable for the log file
+LOG_FILE = "query_log.md"
 
-def query():
-    """Query the database for the top 5 rows of the GroceryDB table"""
-    conn = sqlite3.connect("GroceryDB.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM GroceryDB")
-    print("Top 5 rows of the GroceryDB table:")
-    print(cursor.fetchall())
+
+def log_query(query):
+    """adds to a query markdown file"""
+    with open(LOG_FILE, "a") as file:
+        file.write(f"```sql\n{query}\n```\n\n")
+
+
+def create_record(
+    country, beer_sevrings, spirit_servings, wine_servings, total_pure_alcohol
+):
+    """create example query"""
+    conn = sqlite3.connect("alcoholDB.db")
+    c = conn.cursor()
+    c.execute(
+        """
+        INSERT INTO alcoholDB 
+        (country, beer_sevrings, spirit_servings, wine_servings, total_pure_alcohol) 
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        (country, beer_sevrings, spirit_servings, wine_servings, total_pure_alcohol),
+    )
+    conn.commit()
     conn.close()
-    return "Success"
+
+    log_query(
+        f"""INSERT INTO alcoholDB VALUES (
+                {country}, 
+                {beer_sevrings},
+                {spirit_servings},
+                {wine_servings},
+                {total_pure_alcohol});"""
+    )
 
 
+def read_data():
+    """read data"""
+    conn = sqlite3.connect("alcoholDB.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM alcoholDB")
+    data = c.fetchall()
+    log_query("SELECT * FROM alcoholDB;")
+    return data
+
+
+def update_record(
+            record_id,
+            country, 
+            beer_sevrings,
+            spirit_servings,
+            wine_servings,
+            total_pure_alcohol
+):
+    """update example query"""
+    conn = sqlite3.connect("alcoholDB.db")
+    c = conn.cursor()
+    c.execute(
+        """
+        UPDATE alcoholDB 
+        SET country=?, 
+        beer_sevrings=?, 
+        spirit_servings=?, 
+        wine_servings=?, 
+        total_pure_alcohol=?
+        WHERE id=?
+        """,
+        (
+            country, 
+            beer_sevrings,
+            spirit_servings,
+            wine_servings,
+            total_pure_alcohol,
+            record_id
+        ),
+    )
+    conn.commit()
+    conn.close()
+
+    log_query(
+        f"""UPDATE alcoholDB SET 
+        country={country}, 
+        beer_sevringst=
+        {beer_sevrings},
+        spirit_servings={spirit_servings}, 
+        wine_servings={wine_servings}, 
+        total_pure_alcohol={total_pure_alcohol},
+        WHERE id={record_id};"""
+    )
+
+
+def delete_record(record_id):
+    """delete example query"""
+    conn = sqlite3.connect("alcoholDB.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM alcoholDB WHERE id=?", (record_id,))
+    conn.commit()
+    conn.close()
+
+    log_query(f"DELETE FROM alcoholDB WHERE id={record_id};")
---
+++ query_log.md
@@ -0,0 +1,30 @@
+```sql
+UPDATE alcoholDB SET 
+        country=country_A, 
+        beer_sevringst=
+        10,
+        spirit_servings=10, 
+        wine_servings=10, 
+        total_pure_alcohol=10,
+        WHERE id=3;
+```
+
+```sql
+DELETE FROM alcoholDB WHERE id=1;
+```
+
+```sql
+UPDATE alcoholDB SET 
+        country=country_A, 
+        beer_sevringst=
+        1,
+        spirit_servings=1, 
+        wine_servings=1, 
+        total_pure_alcohol=1.1,
+        WHERE id=1000;
+```
+
+```sql
+SELECT * FROM alcoholDB;
+```
+
