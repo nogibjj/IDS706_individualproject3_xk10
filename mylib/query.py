fb6197cbaef763c8bb222e1c625b4fa123eb22e9
cassiekang <143849077+cassiekang@users.noreply.github.com>
2023-10-10 16:26:33 -0400

    Update query.py

--- mylib/query.py
+++ mylib/query.py
@@ -1,113 +1,34 @@
-"""Query the database"""
+"""Query the database from a db connection to Azure Databricks"""
+import os
+from databricks import sql
+from dotenv import load_dotenv
 
-import sqlite3
 
 # Define a global variable for the log file
 LOG_FILE = "query_log.md"
 
 
-def log_query(query):
+def log_query(query, result="none"):
     """adds to a query markdown file"""
     with open(LOG_FILE, "a") as file:
         file.write(f"```sql\n{query}\n```\n\n")
+        file.write(f"```response from databricks\n{result}\n```\n\n")
 
 
-def create_record(
-    country, confederation, population_share, tv_audience_share, gdp_weighted_share
-):
-    """create example query"""
-    conn = sqlite3.connect("fifaDB.db")
-    c = conn.cursor()
-    c.execute(
-        """
-        INSERT INTO fifaDB 
-        (country, 
-        confederation, 
-        population_share,
-        tv_audience_share, 
-        gdp_weighted_share) 
-        VALUES (?, ?, ?, ?, ?)
-        """,
-        (country, 
-         confederation, 
-         population_share, 
-         tv_audience_share, 
-         gdp_weighted_share),
-    )
-    conn.commit()
-    conn.close()
+def general_query(query):
+    """runs a query a user inputs"""
 
-    log_query(
-        f"""INSERT INTO fifaDB VALUES (
-                {country}, 
-                {confederation},
-                {population_share},
-                {tv_audience_share},
-                {gdp_weighted_share});"""
-    )
-
-
-def read_data():
-    """read data"""
-    conn = sqlite3.connect("fifaDB.db")
-    c = conn.cursor()
-    c.execute("SELECT * FROM fifaDB")
-    data = c.fetchall()
-    log_query("SELECT * FROM fifaDB;")
-    return data
-
-
-def update_record(
-            record_id,
-            country, 
-            confederation, 
-            population_share, 
-            tv_audience_share, 
-            gdp_weighted_share
-):
-    """update example query"""
-    conn = sqlite3.connect("fifaDB.db")
-    c = conn.cursor()
-    c.execute(
-        """
-        UPDATE fifaDB 
-        SET country=?, 
-        confederation=?, 
-        population_share=?, 
-        tv_audience_share=?, 
-        gdp_weighted_share=?
-        WHERE id=?
-        """,
-        (
-            country, 
-            confederation, 
-            population_share, 
-            tv_audience_share, 
-            gdp_weighted_share,
-            record_id
-        ),
-    )
-    conn.commit()
-    conn.close()
-
-    log_query(
-        f"""UPDATE fifaDB SET 
-        country={country}, 
-        confederation=
-        {confederation},
-        population_share={population_share}, 
-        tv_audience_share={tv_audience_share}, 
-        gdp_weighted_share={gdp_weighted_share},
-        WHERE id={record_id};"""
-    )
-
-
-def delete_record(record_id):
-    """delete example query"""
-    conn = sqlite3.connect("fifaDB.db")
-    c = conn.cursor()
-    c.execute("DELETE FROM fifaDB WHERE id=?", (record_id,))
-    conn.commit()
-    conn.close()
-
-    log_query(f"DELETE FROM fifaDB WHERE id={record_id};")
+    load_dotenv()
+    server_h = os.getenv("SERVER_HOSTNAME")
+    access_token = os.getenv("ACCESS_TOKEN")
+    http_path = os.getenv("HTTP_PATH")
+    with sql.connect(
+        server_hostname=server_h,
+        http_path=http_path,
+        access_token=access_token,
+    ) as connection:
+        c = connection.cursor()
+        c.execute(query)
+        result = c.fetchall()
+    c.close()
+    log_query(f"{query}", result)
